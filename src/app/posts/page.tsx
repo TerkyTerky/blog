41367fd2b008dbdfb8168607e3f1af67ed8e5013
wulikunkun <1243744220@qq.com>
2025-11-11 12:36:11 +0800

    feat: 新增摄影专栏

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -3,7 +3,6 @@ import { redirect } from 'next/navigation'
 
 export default function PostPage() {
   // 使用第一个文章的 slug 作为默认值
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const defaultSlug = postFileTreeList[0]?.slug
   if (!defaultSlug) {
     throw new Error('No posts found')
